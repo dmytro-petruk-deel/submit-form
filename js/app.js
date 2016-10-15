2bddb9dce6fb7538dcc18687a358c48aa1edb203
petruk-dmitriy <dmitpet2010@mail.ru>
2016-10-15 13:34:07 +0300

    Created Header component.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header.js';
+import style from '../scss/screen.scss';
+
+ReactDOM.render(<Header/>, document.getElementById('app'));
---
+++ js/components/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="number">3/4</div>
+        <h1 clasNames="main-title">Who is booking?</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;

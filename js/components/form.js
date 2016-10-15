59ed2206da2d049b56a44c32b36c27a7e537c0f7
petruk-dmitriy <dmitpet2010@mail.ru>
2016-10-15 14:08:50 +0300

    Part of functional to display info.

--- js/components/form.js
+++ js/components/form.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
 class InputText extends Component {
-
   render() {
     return (
       <div className="form-item field-type-textfield">
         <label htmlFor="field-user-phone">Fill address by user phone</label>
-        <input type="tel" id="field-user-phone" className="form-text" placeholder="+4526332211"/>
+        <input type="tel" id="field-user-phone" className="form-text" placeholder="+4526332211"
+               value={this.props.value}/>
       </div>
     )
   }
@@ -16,20 +16,33 @@ class InputSubmit extends Component {
   render() {
     return (
       <div className="form-actions">
-        <input type="submit" className="form-submit" value="Find Information"/>
+        <input type="submit" className="form-submit" value="Find Information" onClick={this.props.onClick}/>
       </div>
     )
   }
 }
 
-
 class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleSubmit(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
   render() {
     return (
-      <form className="submit-form">
-        <InputText />
-        <InputSubmit />
-      </form>
+      <div className="submit-form">
+        <InputText/>
+        <InputSubmit onClick={this.handleSubmit.bind(this)}/>
+      </div>
     )
   }
 }
